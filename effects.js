5e342feaa2ba0d928b342c8cab194f19b33e4bc0
root <root@localhost.localdomain>
2020-03-30 18:39:27 +0530

    new changes

---
+++ effects.js
@@ -0,0 +1,9 @@
+function o_menu(){
+    document.getElementById("mySidebar").style.width = "17%";
+    document.getElementById("main").style.marginLeft = "17%";
+  }
+  
+  function c_menu() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
