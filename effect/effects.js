7cb074f11a9d9530683b681e09fa9b22796a905c
root <root@localhost.localdomain>
2020-03-31 19:05:54 +0530

    changes

--- effect/effects.js
+++ effect/effects.js
@@ -1,9 +1,26 @@
 function o_menu(){
-    document.getElementById("mySidebar").style.width = "17%";
-    document.getElementById("main").style.marginLeft = "17%";
-  }
+  document.getElementById("mySidebar").style.width = "14%";
+  setTimeout(function(){ 
+  let arr = Array.from(document.querySelectorAll('.changeme'));
+  arr.forEach( e=> e.classList.remove('visibility')); 
+  }, 350);
   
-  function c_menu() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+  // document.getElementById("main").style.marginLeft = "17%";
+}
+
+function c_menu() {
+  let arr = Array.from(document.querySelectorAll('.changeme'));
+  arr.forEach( e=> e.classList.add('visibility'));
+  document.getElementById("mySidebar").style.width = "0";
+  // document.getElementById("main").style.marginLeft= "0";
+}
+function createSignOut()
+{
+  document.getElementById("signOut-btn").style.display="block";
+}
+function blurEffect(val)
+{
+  const dom_con=document.getElementById(val);
+  dom_con.classList.remove('blurme');
+  document.getElementById('cntr-box').style.display='none';
+}
